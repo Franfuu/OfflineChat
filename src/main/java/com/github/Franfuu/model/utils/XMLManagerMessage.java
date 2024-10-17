33e5dcea730a767b3dfe21b601ab43aa45de81a8
JuanMa748 <junperz163@gmail.com>
2024-10-17 13:53:00 +0200

    Creados el login, el welcome y el register. Aparte creadas las entidades user, message

---
+++ src/main/java/com/github/Franfuu/model/utils/XMLManagerMessage.java
@@ -0,0 +1,43 @@
+package com.github.Franfuu.model.utils;
+import com.github.Franfuu.model.entity.Message;
+import com.github.Franfuu.model.entity.User;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+public class XMLManagerMessage {
+
+        public static boolean writeXML(Message ms, String fileName) {
+            boolean result = false;
+            JAXBContext context;
+            try {
+                context = JAXBContext.newInstance(ms.getClass());
+                Marshaller m = context.createMarshaller();
+                m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+                m.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+                m.marshal(ms, new File(fileName));
+                result = true;
+            } catch (JAXBException e) {
+                e.printStackTrace(); //mode development
+            }
+            return result;
+        }
+
+        public static Message readXML(User u, String filename) {
+            User result = u;
+            JAXBContext context;
+            try {
+                context = JAXBContext.newInstance(u.getClass());
+                Unmarshaller um = context.createUnmarshaller();
+                result = (User) um.unmarshal(new File(filename));
+            } catch (JAXBException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+    }
+
+
