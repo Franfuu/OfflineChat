42ad52b715b6fde6c3f84baa2f46675c9a753a21
JuanMa748 <junperz163@gmail.com>
2024-10-23 09:24:34 +0200

    Hacer el css del Chat

---
+++ src/main/java/com/github/Franfuu/test/testCSS.java
@@ -0,0 +1,13 @@
+package com.github.Franfuu.test;
+
+import com.github.Franfuu.view.AppController;
+import com.github.Franfuu.view.Scenes;
+import com.github.Franfuu.view.View;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class testCSS{
+
+
+}
